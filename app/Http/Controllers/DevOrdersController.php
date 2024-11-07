1ebd1950389b21cabf2a8f71eaf07a13cbafa0c5
kurisbd <mdammad75@gmail.com>
2024-11-07 11:49:52 +0600

    dev order file

---
+++ app/Http/Controllers/DevOrdersController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DevOrders;
+use App\Http\Requests\StoreDevOrdersRequest;
+use App\Http\Requests\UpdateDevOrdersRequest;
+
+class DevOrdersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDevOrdersRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DevOrders $devOrders)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DevOrders $devOrders)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDevOrdersRequest $request, DevOrders $devOrders)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DevOrders $devOrders)
+    {
+        //
+    }
+}
